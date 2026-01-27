530751f813a4290c4e9b1021dab615c2794adbb0
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-01-27 17:41:32 -0500

    Updated Icon

--- ZEngine/obj/Debug/net8.0/ZEngine.AssemblyInfo.cs
+++ ZEngine/obj/Debug/net8.0/ZEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cf8ebad7b4e83f8928d6eec8b6788f2e3ca35f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a690807b79957acd23ba5378e544c1ce4a787a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
